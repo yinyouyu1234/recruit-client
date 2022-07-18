517df4f9e745f26126cc19b3a7cf6d5c60bdeaf8
yinyouyu <yin_you_yu@163.com>
2022-07-18 11:02:23 +0800

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,33 +4,41 @@
     <div class="top_btn" @click="changeLangEvent">中文</div>
     kk
     <div style="width: 800px">
-        <swiper
-          :space-between="50"
-          @swiper="onSwiper"
-          @slideChange="onSlideChange"
-        >
-          <swiper-slide>Slide 1</swiper-slide>
-          <swiper-slide>Slide 2</swiper-slide>
-          <swiper-slide>Slide 3</swiper-slide>
-          <swiper-slide>Slide 3</swiper-slide>
-          <swiper-slide>Slide 3</swiper-slide>
-          <swiper-slide>Slide 3</swiper-slide>
-          <swiper-slide>Slide 3</swiper-slide>
-          <swiper-slide>Slide 3</swiper-slide>
-        </swiper>
+    <div class="swiper mySwiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">Slide 1</div>
+        <div class="swiper-slide">Slide 2</div>
+        <div class="swiper-slide">Slide 3</div>
+        <div class="swiper-slide">Slide 4</div>
+        <div class="swiper-slide">Slide 5</div>
+        <div class="swiper-slide">Slide 6</div>
+        <div class="swiper-slide">Slide 7</div>
+        <div class="swiper-slide">Slide 8</div>
+        <div class="swiper-slide">Slide 9</div>
+      </div>
+      <div class="swiper-pagination"></div>
+    </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { Swiper, SwiperSlide } from 'swiper/vue';
+import Swiper from 'swiper'
+// import { Swiper, SwiperSlide } from 'swiper/vue';
   import 'swiper/css';
 export default defineComponent({
   name: 'HomeView',
-      components: {
-      Swiper,
-      SwiperSlide,
+    //   components: {
+    //   Swiper,
+    //   SwiperSlide,
+    // },
+    mounted() {
+            var swiper = new Swiper(".mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+        },
+      });
     },
   methods: {
     onSlideChange() {
